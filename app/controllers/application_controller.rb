67b7e17f4700dbfd412406ac5b64c66c06647c51
kerrieyee <kerrie@kerrieyee.com>
2012-11-11 14:47:21 -0800

    filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter do 
+
+  	@upcase_titles = @article.title.upcase
+  end
+
+  private
+
+  def load_article 
+  	@article = Article.find(params[:id]) if params[:id]
+  end
+
+  def upcase_article_titles
+  	@article.title = @article.title.capitalize
+  end
 end
